61cdd83bfa291323cd24f47eaf5922528242a0c0
OcarinGit <oscarsantanaalvarez@gmail.com>
2019-12-12 14:15:21 -0600

    Rest client for POST requests

---
+++ src/com/exercise45restclient2/app/RestClient2App.java
@@ -0,0 +1,32 @@
+package com.exercise45restclient2.app;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.exercise45restclient2.model.Product;
+
+public class RestClient2App {
+
+	public static void main(String[] args) {
+		Client client = ClientBuilder.newClient();
+		 
+		WebTarget webTarget = client.target("http://localhost:8080/Exercise45Rest/").path("Products");
+		 
+		Invocation.Builder invocationBuilder =  webTarget.request(MediaType.APPLICATION_JSON);
+		 
+		Product myProduct = new Product();
+		myProduct.setIdProduct(1);
+		myProduct.setNameProduct("Jocoque");
+		myProduct.setPriceProduct(40.5);
+		Response response = invocationBuilder.post(Entity.entity(myProduct, MediaType.APPLICATION_JSON));
+		System.out.println(response.getStatus());
+		System.out.println(response.readEntity(String.class));
+
+	}
+
+}
---
+++ src/com/exercise45restclient2/model/Product.java
@@ -0,0 +1,46 @@
+package com.exercise45restclient2.model;
+
+public class Product {
+	private int idProduct;
+	private String nameProduct;
+	private double priceProduct;
+	
+	public Product() {
+	}
+	public Product(int idProduct, String nameProduct, double priceProduct)
+	{
+		this.idProduct = idProduct;
+		this.nameProduct = nameProduct;
+		this.priceProduct = priceProduct;
+	}
+
+	public int getIdProduct()
+	{
+		return this.idProduct;
+	}
+	public void setIdProduct(int idProduct)
+	{
+		this.idProduct = idProduct;
+	}
+	public String getNameProduct()
+	{
+		return this.nameProduct;
+	}
+	public void setNameProduct(String nameProduct)
+	{
+		this.nameProduct = nameProduct;
+	}
+	public double getPriceProduct()
+	{
+		return this.priceProduct;
+	}
+	public void setPriceProduct(double priceProduct)
+	{
+		this.priceProduct = priceProduct;
+	}
+	@Override
+	public String toString()
+	{
+		return "Id Product:"+this.idProduct+", Name Product:"+this.nameProduct+", Price Product:"+this.priceProduct;
+	}
+}
